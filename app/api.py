8714d9357bf0547f29808e2b05b354e94d0b4267
amirmahdi <152201102+Amirelvx11@users.noreply.github.com>
2025-11-15 09:24:58 +0330

    rename app title based on our projects

--- app/api.py
+++ app/api.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from .rag import rag_pipeline
 
-app = FastAPI(title="Salona Bot Task", version="1.0")
+app = FastAPI(title="Simple Rag Pipeline for LLM Project - Simulate Dm Messages", version="1.0")
 
 class DMRequest(BaseModel):
     sender_id: str
