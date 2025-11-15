630bb1931267310d45d43b5dfdb7c6b3096bd54f
amirmahdi <152201102+Amirelvx11@users.noreply.github.com>
2025-11-15 09:11:40 +0330

    last touch inside main module

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+''' 
+# for Runnig this application please first populate the db and then run this command in your terminal
+# (make sure that you have installed requirements & python packages
+uvicorn main:app --reload --port 8000
+'''
 from app.api import app
-
-# Run the application using:
-# uvicorn main:app --reload --port 8000
